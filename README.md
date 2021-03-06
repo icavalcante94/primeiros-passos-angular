24be1aefb8e2c14a565559104285d520eef2711c
icavalcante94 <icavalcante@uea.edu.br>
2021-03-06 09:28:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeiros-passos-angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeiros-passos-angular)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'Um grande telefone com uma das melhores telas'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'Um ótimo telefone com uma das melhores câmeras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: 'Tela com cantos arredondados que seguem um belo design curvo'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
